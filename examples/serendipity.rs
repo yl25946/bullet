f09ffe4b2374e6e5488fd96027e226b6af5869e6
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2024-09-10 19:30:27 -0500

    added serendipity net config

---
+++ examples/serendipity.rs
@@ -0,0 +1,66 @@
+use bullet_lib::{
+    inputs, loader, lr, optimiser, outputs, wdl, Activation, LocalSettings, Loss, TrainerBuilder, TrainingSchedule,
+};
+const HIDDEN_SIZE: usize = 1536;
+const QA: i32 = 255;
+const QB: i32 = 64;
+
+fn main() {
+    let mut trainer = TrainerBuilder::default()
+        .quantisations(&[QA, QB])
+        .optimiser(optimiser::AdamW)
+        .input(inputs::ChessBucketsMirrored::new([
+            0, 0, 1, 1,
+            2, 2, 2, 2,
+            3, 3, 3, 3,
+            3, 3, 3, 3,
+            3, 3, 3, 3,
+            3, 3, 3, 3,
+            3, 3, 3, 3,
+            3, 3, 3, 3,
+        ]))
+        .output_buckets(outputs::MaterialCount::<8>::default())
+        .feature_transformer(HIDDEN_SIZE)
+        .activate(Activation::SCReLU)
+        .add_layer(1)
+        .build();
+
+    let schedule = TrainingSchedule {
+        net_id: "serendipity".to_string(),
+        eval_scale: 400.0,
+        ft_regularisation: 0.0,
+        batch_size: 16_384,
+        batches_per_superbatch: 6104,
+        start_superbatch: 1,
+        end_superbatch: 400,
+        wdl_scheduler: wdl::ConstantWDL { value: 0.0 },
+        lr_scheduler: lr::StepLR { start: 0.001, gamma: 0.992, step: 1 },
+        loss_function: Loss::SigmoidMSE,
+        save_rate: 400,
+        optimiser_settings: optimiser::AdamWParams {
+            decay: 0.01,
+            beta1: 0.9,
+            beta2: 0.999,
+            min_weight: -1.98,
+            max_weight: 1.98,
+        },
+    };
+
+    let settings = LocalSettings { threads: 4, test_set: None, output_directory: "checkpoints", batch_queue_size: 512 };
+
+    let data_loader = loader::DirectSequentialDataLoader::new(&["../NNUE Data/master-dataset.bin"]);
+
+    trainer.run(&schedule, &settings, &data_loader);
+
+    for fen in [
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+        "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+        "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
+        "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
+        "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1",
+    ] {
+        let eval = trainer.eval(fen);
+        println!("FEN: {fen}");
+        println!("EVAL: {}", 400.0 * eval);
+    }
+}
