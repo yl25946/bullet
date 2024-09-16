8cbf8966abad828e224d979e261d96186c00b0c3
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2024-09-16 15:20:48 -0500

    try a different bucket layout

--- examples/smolnet.rs
+++ examples/smolnet.rs
@@ -21,10 +21,10 @@ fn main() {
         .quantisations(&[QA, QB])
         .optimiser(optimiser::AdamW)
         .input(inputs::ChessBucketsMirrored::new([
-            0, 0, 1, 1,
-            0, 2, 2, 2,
-            3, 3, 3, 3,
-            3, 3, 3, 3,
+            0, 0, 0, 1,
+            0, 2, 1, 1,
+            2, 2, 3, 3,
+            2, 3, 3, 3,
             3, 3, 3, 3,
             3, 3, 3, 3,
             3, 3, 3, 3,
@@ -62,6 +62,18 @@ fn main() {
     let data_loader = loader::DirectSequentialDataLoader::new(&["../NNUE Data/master-dataset.bin"]);
 
     trainer.run(&schedule, &settings, &data_loader);
+
+    for fen in [
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+        "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+        "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
+        "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
+        "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1",
+    ] {
+        let eval = trainer.eval(fen);
+        println!("FEN: {fen}");
+        println!("EVAL: {}", 400.0 * eval);
+    }
 }
 
 /*
