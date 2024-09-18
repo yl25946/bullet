c9e76e0e680ec562e95ddb2368f20f6b1f13268d
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2024-09-18 00:12:00 -0500

    NEW CONFIG

--- examples/smolnet.rs
+++ examples/smolnet.rs
@@ -21,10 +21,10 @@ fn main() {
         .quantisations(&[QA, QB])
         .optimiser(optimiser::AdamW)
         .input(inputs::ChessBucketsMirrored::new([
-            0, 1, 2, 3,
-            4, 4, 4, 4,
-            4, 4, 4, 4,
-            4, 4, 4, 4,
+            0, 0, 1, 1,
+            0, 2, 2, 2,
+            3, 3, 4, 4,
+            3, 3, 4, 4,
             4, 4, 4, 4,
             4, 4, 4, 4,
             4, 4, 4, 4,
