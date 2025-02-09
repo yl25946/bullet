b2316490e26338a06cf0619ed208d83fec4081a6
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2025-02-08 20:51:14 -0600

    testing out one set of hypepararms

--- examples/retrain.rs
+++ examples/retrain.rs
@@ -14,7 +14,7 @@ const SCALE: i32 = 400;
 const QA: i32 = 255;
 const QB: i32 = 64;
 
-const SUPERBATCHES: usize = 400;
+const SUPERBATCHES: usize = 100;
 
 fn main() {
     let mut trainer = TrainerBuilder::default()
@@ -27,8 +27,11 @@ fn main() {
         .add_layer(1)
         .build();
 
+        
+        trainer.load_from_checkpoint("checkpoints/2048-8-quadratic-relabeled-stockfish-4096-400");
+
     let schedule = TrainingSchedule {
-        net_id: "2048-8-quadratic".to_string(),
+        net_id: "2048-8-quadratic-relabeled-stockfish-retrained-leela".to_string(),
         eval_scale: 400.0,
         ft_regularisation: 0.0,
         batch_size: 16_384,
@@ -36,9 +39,9 @@ fn main() {
         start_superbatch: 1,
         end_superbatch: SUPERBATCHES,
         wdl_scheduler: wdl::ConstantWDL { value: 0.0 },
-        lr_scheduler: lr::StepLR { start: 0.001, gamma: 0.3, step: SUPERBATCHES / 4 },
+        lr_scheduler: lr::StepLR { start: 0.000001, gamma: 0.3, step: SUPERBATCHES },
         loss_function: Loss::SigmoidMSE,
-        save_rate: SUPERBATCHES / 4,
+        save_rate: SUPERBATCHES,
         optimiser_settings: optimiser::AdamWParams {
             decay: 0.01,
             beta1: 0.9,
@@ -50,7 +53,7 @@ fn main() {
 
     let settings = LocalSettings { threads: 4, test_set: None, output_directory: "checkpoints", batch_queue_size: 512 };
 
-    let data_loader = loader::DirectSequentialDataLoader::new(&["../NNUE Data/master-dataset-relabeled-stockfish-4096.bin"]);
+    let data_loader = loader::DirectSequentialDataLoader::new(&["../NNUE Data/master-dataset.bin"]);
 
     trainer.run(&schedule, &settings, &data_loader);
 }
